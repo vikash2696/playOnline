19e0bd276966d31f6bf0fd077b2f2e84979c0929
vikash kumar <vikashkumar@vikashs-MacBook-Air.local>
2026-05-16 22:42:19 +0530

    package.json updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { cloudflare } from "@cloudflare/vite-plugin"
 
 export default defineConfig({
   plugins: [react(), cloudflare()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
 })
